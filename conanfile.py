16e78d9f2b5e330316f9b2e66b1f65ee4dc67ea5
那么强大 <zpg1995@126.com>
2025-07-10 21:19:40 +0800

    第一次修改drogon

---
+++ conanfile.py
@@ -0,0 +1,21 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+
+class ExampleRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        # self.requires("drogon/1.9.10")
+        self.requires("jsoncpp/1.9.4")
+        self.requires("zlib/1.2.11")
+        # self.requires("gtest/1.10.0")
+        self.requires("sqlite3/3.40.1")
+        # self.requires("libpq/13.2")
+        self.requires("openssl/1.1.1t")
+        # self.requires("hiredis/1.0.0")
+        self.requires("brotli/1.0.9")
+
+    # def layout(self):
+    #     cmake_layout(self)
